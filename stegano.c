16bbc84f508b95de811f527d6ac54537f90b61f7
jinze1994 <baijinze1994@163.com>
2017-02-08 14:31:51 +0800

    add coeffsToStuckBitStream

--- stegano.c
+++ stegano.c
@@ -14,17 +14,42 @@ static bool checkJPEGScale(struct jpeg_decompress_struct* cinfo) {
 	return true;
 }
 
-static int getValidCoeffs(size_t blocks, uint32_t** ptr, size_t* n) {
+static uint32_t* getValidCoeffs(size_t blocks, size_t* n) {
 	uint32_t* coeffs = (uint32_t*) malloc(blocks * sizeof(uint32_t));
-	if (coeffs == NULL) return 20;
+	if (coeffs == NULL) return NULL;
 
 	for (size_t i = 0; i < blocks; i++)
-		coeffs[i] = i * 64 + 1;
+		coeffs[i] = i * DCTSIZE2 + 1;
 
-	*ptr = coeffs;
 	*n = blocks;
-	return 0;
+	return coeffs;
 }
+
+static uint8_t* coeffsToStuckBitStream(struct jpeg_decompress_struct* cinfo,
+	jvirt_barray_ptr* coeff_array, uint32_t component_id,
+	uint32_t* coeffs, size_t coeffs_len) {
+
+	uint8_t* stream = malloc(coeffs_len * sizeof(uint8_t));
+	if (stream == NULL) return NULL;
+
+	for (size_t i = 0; i < coeffs_len; i++) {
+		uint32_t block_id = coeffs[i] / DCTSIZE2, k = coeffs[i] % DCTSIZE2;
+		uint32_t block_x = block_id / cinfo->comp_info[component_id].width_in_blocks;
+		uint32_t block_y = block_id % cinfo->comp_info[component_id].width_in_blocks;
+		JBLOCKARRAY B = (cinfo->mem -> access_virt_barray)((j_common_ptr)cinfo,
+				coeff_array[component_id],
+				block_x, 1, FALSE);
+		JCOEFPTR dctblck = B[0][block_y];
+		JCOEF coef = dctblck[k];
+
+		if (coef == 0)
+			stream[i] = 0;
+		else
+			stream[i] = 1;
+	}
+	return stream;
+}
+
 
 int steganoEncode(FILE* infile, FILE* outfile,
 	const char* message, const char* password) {
@@ -56,12 +81,11 @@ int steganoEncode(FILE* infile, FILE* outfile,
 		blocks = w * h / 64;
 	}
 
-	int rv;
 	uint32_t *coeffs; size_t coeffs_len;
-	rv = getValidCoeffs(blocks, &coeffs, &coeffs_len);
-	if (rv) {
+	coeffs = getValidCoeffs(blocks, &coeffs_len);
+	if (coeffs == NULL) {
 		jpeg_destroy_decompress(&cinfo_in);
-		return rv;
+		return 20;
 	}
 
 	{
@@ -71,6 +95,16 @@ int steganoEncode(FILE* infile, FILE* outfile,
 		rgen_free(&rge);
 	}
 
+	uint8_t* stream = coeffsToStuckBitStream(&cinfo_in, 
+			luma_coeff_array, 0,
+			coeffs, coeffs_len);
+	if (stream == NULL) {
+		free(coeffs);
+		jpeg_destroy_decompress(&cinfo_in);
+		return 20;
+	}
+
+	free(stream);
 	free(coeffs);
 	jpeg_destroy_decompress(&cinfo_in);
 	return 0;
